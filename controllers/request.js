47956159694bfb65143e5ce80ab54f9a80673921
HAZEM bensaid <hazembensaid195@gmail.com>
2021-12-30 23:57:19 +0100

    rr

--- controllers/request.js
+++ controllers/request.js
@@ -82,7 +82,7 @@ exports.getReceivedRequest =  async(req, res) => {
            
             const newuser={...user , roomMates : user.roomMates.push(req.user._id)}
             const receiveuser={...req.user,roomMates : req.user.roomMates.push(user._id)}
-             user.roomMates.map((el)=>  {receiveuser={...receiveuser, roomMates : el.roomMates.push(user._id)}})
+             if(user.roomMates.length>0) {user.roomMates.map((el)=>  {receiveuser={...receiveuser, roomMates : el.roomMates.push(user._id)}})}
             const resu = await User.updateOne(
                 { _id: user._id },
                 { $set: { ...newuser } }

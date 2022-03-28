a654fe913f9682cadc2cdcf2a97aac0f4a1b36a0
Hazem <hazembensaid195@gmail.com>
2022-03-28 08:42:33 +0100

    lll

---
+++ script.groovy
@@ -0,0 +1,40 @@
+def buildImage(String imageName) {
+    echo 'building the docker image...'
+    //getting credentials of github from jenkins
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        //building the image with dockerhub repo tag
+        sh "docker build -t $imageName ."
+        //login to dockerhub
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        //pushing the image to dockerhub
+        sh "docker push $imageName "
+    }
+} 
+def test(String imageName) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        //login to dockerhub
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker pull $imageName "
+
+        echo 'testing the application...'
+    }
+}
+def release(String imageName) {
+     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        //login to dockerhub
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker pull $imageName "
+
+        sh "docker tag  $imageName hazem06/ncsc_backend:node-app-release-1.0"
+        sh "docker push hazem06/ncsc_backend:node-app-release-1.0 "
+    }
+}
+def deployApp(String imageName) {
+     
+    
+      echo 'deploying the appppllication'
+
+    
+} 
+
+return this

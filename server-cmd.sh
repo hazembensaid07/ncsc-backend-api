206ff9cac6ffcf32315b7a2b52565b9555bbeb34
Hazem <hazembensaid195@gmail.com>
2022-03-31 07:57:32 +0100

    eee

--- server-cmd.sh
+++ server-cmd.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export IMAGE=$1
-docker-compose -f docker-compose.yml up --detach --build
+docker-compose -f docker-compose.yml up --scale app=2  --detach --build
 echo "success"

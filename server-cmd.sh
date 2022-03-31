a66bc644f39759d262bab3baaeaa9d01765fca87
Hazem <hazembensaid195@gmail.com>
2022-03-31 09:45:49 +0100

    eee

--- server-cmd.sh
+++ server-cmd.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export IMAGE=$1
-docker image rm hazem06/ncsc_test:node-app-release-1.0 
+
 docker-compose -f docker-compose.yml down  
+docker image rm -f hazem06/ncsc_test:node-app-release-1.0 
 docker-compose -f docker-compose.yml up --scale app=2  --detach 
 echo "success"
